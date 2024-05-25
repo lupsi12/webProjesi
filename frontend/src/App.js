64b3c3719a990b4f26c9072df7af09db43d5065f
isil kefal <kefalisil9@gmail.com>
2024-05-25 17:26:58 +0300

    Cokkie Management,REACT html/css,RBAC

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,22 @@ import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import User from "./pages/User";
+import Logout from "./pages/Logout";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/admin" element={<Admin/>}/>
-            <Route path="/" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/user" element={<User/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/admin" element={<Admin/>}/>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/user" element={<User/>}/>
+                    <Route path="/logout" element={<Logout/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
